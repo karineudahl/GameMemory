53f94a446eb02ea89bd1e2dd65de336944ece756
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-03-17 18:39:40 +0100

    c  sd  c ds

--- js/script.js
+++ js/script.js
@@ -69,4 +69,21 @@ function shuffleCard() {
     }); 
 }
 
-shuffleCard()
+shuffleCard();
+
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+  span.addEventListener('click', (e) => {
+    e.target.classList.add('active');
+  });
+  span.addEventListener('animationend', (e) => {
+    e.target.classList.remove('active');
+  });
+  
+  // Initial animation
+  setTimeout(() => {
+    span.classList.add('active');
+  }, 750 * (idx+1))
+});
