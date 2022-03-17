7e41592c336e7bc32f1dd83a2f36a89528d850ad
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-03-17 19:02:49 +0100

    update

--- js/script.js
+++ js/script.js
@@ -72,18 +72,3 @@ function shuffleCard() {
 shuffleCard();
 
 
-const spans = document.querySelectorAll('.word span');
-
-spans.forEach((span, idx) => {
-  span.addEventListener('click', (e) => {
-    e.target.classList.add('active');
-  });
-  span.addEventListener('animationend', (e) => {
-    e.target.classList.remove('active');
-  });
-  
-  // Initial animation
-  setTimeout(() => {
-    span.classList.add('active');
-  }, 750 * (idx+1))
-});
